debba0902ea0907ab537bcd6fa2832eb91516bec
vomiter <resetvomiter@gmail.com>
2025-11-30 01:13:29 +0800

    1.0.1

--- src/main/java/com/vomiter/survivorsaquaculture/mixin/neptunium/NeptuniumArmor_EffectMixin.java
+++ src/main/java/com/vomiter/survivorsaquaculture/mixin/neptunium/NeptuniumArmor_EffectMixin.java
@@ -23,7 +23,7 @@ public abstract class NeptuniumArmor_EffectMixin extends ArmorItem {
         super(p_40386_, p_266831_, p_40388_);
     }
 
-    @WrapOperation(method = "onArmorTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;addEffect(Lnet/minecraft/world/effect/MobEffectInstance;)Z", ordinal = 0))
+    @WrapOperation(method = "onArmorTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Player;addEffect(Lnet/minecraft/world/effect/MobEffectInstance;)Z", ordinal = 0, remap = true))
     private boolean swapNightVisionEffect(Player instance, MobEffectInstance mobEffectInstance, Operation<Boolean> original){
         return instance.addEffect(new MobEffectInstance(SAEffects.SENSES.get(SenseEffect.SenseType.NEPTUNE).get(), 20, 0, false, false, false));
     }
